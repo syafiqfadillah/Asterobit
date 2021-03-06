0e6283867d64dba59fea4c49c2abac15e1841aca
syafiqfadillah <codingan77@gmail.com>
2021-03-06 13:18:12 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import things
+
+from ursina import *
+
+
+app = Ursina()
+
+# window setting
+window.title = "asterobit"
+window.size = (600, 500)
+window.borderless = False
+window.exit_button.enabled = False
+
+# create entity
+player = things.Player()
+enemy = things.Enemy()
+powerful_enemy = things.PowerfulEnemy()
+
+# first append
+enemy_entity = []
+enemy_entity.append(enemy)
+enemy_entity.append(powerful_enemy)
+
+def update():
+    global enemy
+    global powerful_enemy
+
+    player.movement()
+
+    for entity in enemy_entity:
+        entity.movement()
+
+        player.collid(entity)
+    
+    line_appeared_enemy = random.randint(-4, -1)
+    if enemy.y <= line_appeared_enemy:
+        enemy = enemy.generate()
+        enemy_entity.append(enemy)
+    
+    if powerful_enemy.y <= line_appeared_enemy:
+        powerful_enemy = powerful_enemy.generate()
+        powerful_enemy.transformation()
+        enemy_entity.append(powerful_enemy)
+
+app.run()
---
+++ things.py
@@ -0,0 +1,88 @@
+import sys
+import random
+
+from ursina import *
+from ursina.prefabs.health_bar import HealthBar
+
+
+class Player(Entity):
+    def __init__(
+        self,
+        model="cube",
+        scale=(1.5, 0.9, 0),
+        color=color.black,
+        position=(0, -6, 0),
+        collider="box",
+    ):
+        super().__init__()
+        self.model = model
+        self.scale = scale
+        self.color = color
+        self.position = position
+        self.collider = collider
+        self.health = HealthBar(roundness=.5, value=50)
+
+    def movement(self):
+        if held_keys["up arrow"]:
+            self.y += 0.08
+        elif held_keys["down arrow"]:
+            self.y -= 0.08
+        elif held_keys["right arrow"]:
+            self.x += 0.08
+        elif held_keys["left arrow"]:
+            self.x -= 0.08
+
+    def collid(self, other):
+        hit_info = self.intersects()
+        if hit_info.hit:
+            if hit_info.entity == other:
+                self.health.value -= 10
+
+                if self.health.value == 0:
+                    self.enabled = False
+
+
+class Enemy(Entity):
+    def __init__(
+        self,
+        model="cube",
+        scale=(1.5, 0.9, 0),
+        color=color.white,
+        position=(0, 6, 0),
+        collider="box",
+    ):
+        super().__init__()
+        self.model = model
+        self.scale = scale
+        self.color = color
+        self.position = position
+        self.collider = collider
+
+    def movement(self):
+        self.y -= 0.08
+
+    def generate(self):
+        spawn_rate_pos = (random.randint(-6, 5), 6)
+        other = duplicate(self, position=spawn_rate_pos)
+
+        return other
+
+
+class PowerfulEnemy(Enemy):
+    def __init__(
+        self,
+        model="circle",
+        scale=(2.5, 1.9, 0),
+        color=color.red,
+        position=(0, 4, 0),
+        collider="box",
+    ):
+        super().__init__(
+            model=model, scale=scale, color=color, position=position, collider=collider
+        )
+    
+    def transformation(self):
+        shape = ["circle", "cube"]
+        choice_shape = random.choice(shape)
+        
+        self.model = choice_shape
